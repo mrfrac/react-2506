51db8fda7f1aba046f668f639b693184586762d8
mrfrac <fracturer@gmail.com>
2025-07-04 15:58:51 +0500

    show restaurants list

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,42 @@
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
+import { restaurants } from './mock';
 
 createRoot(document.getElementById('root')!).render(
-    createElement('div', {}, 'Hello world')
+    createElement(
+        'div',
+        {},
+        restaurants.map((resto) => {
+            return createElement('div', { key: resto.id }, [
+                createElement('h1', { key: 'restoName' }, resto.name),
+                createElement('h3', { key: 'restoMenu' }, 'Меню'),
+                createElement(
+                    'ul',
+                    { key: 'restoMenuList' },
+                    resto.menu.map((menuItem) =>
+                        createElement('li', { key: menuItem.id }, [
+                            menuItem.name,
+                            ` (${menuItem.ingredients.join(', ')})`,
+                            ': ',
+                            menuItem.price,
+                        ])
+                    )
+                ),
+                createElement('h3', { key: 'restoReviews' }, 'Отзывы'),
+                createElement(
+                    'ul',
+                    { key: 'restoReviewsList' },
+                    resto.reviews.map((review) =>
+                        createElement('li', { key: review.id }, [
+                            review.user,
+                            ' ',
+                            review.text,
+                            ` (${review.rating})`,
+                        ])
+                    )
+                ),
+                createElement('hr', { key: 'delimiter' }),
+            ]);
+        })
+    )
 );
