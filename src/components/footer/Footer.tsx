6032ebeb81795dd973ad4276d3ddc408fb059991
mrfrac <fracturer@gmail.com>
2025-07-13 11:42:27 +0500

    feat: scroll progress

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,12 +5,13 @@ export const Footer = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        position: 'absolute',
+        position: 'fixed',
         left: 0,
         bottom: 0,
         width: '100%',
         border: '1px dashed #ccc',
         padding: '16px',
+        backgroundColor: '#fff',
       }}
     >
       &copy; Footer
