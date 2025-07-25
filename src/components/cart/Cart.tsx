e8e64498ca0fb6daee8e04b518f2fac48c6073f7
mrfrac <fracturer@gmail.com>
2025-07-25 22:36:26 +0500

    hw-8: Remove empty cart check in Cart component

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -6,10 +6,6 @@ import { CartItem } from './CartItem.tsx';
 export const Cart = () => {
   const cartItems = useSelector(selectCartItemsIds);
 
-  if (cartItems.length === 0) {
-    return <span>Пусто</span>;
-  }
-
   return (
     <div className={styles.cart}>
       {cartItems.map((cartItemId) => (
